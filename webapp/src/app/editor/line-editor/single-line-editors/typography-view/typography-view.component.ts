efc99d11be53b8da35c7b9e3065e0fb75baa2f83
Christoph Wick <wick.chr.info@gmail.com>
2019-04-04 09:34:27 +0200

    fixed typography view to show petit and super

--- webapp/src/app/editor/line-editor/single-line-editors/typography-view/typography-view.component.ts
+++ webapp/src/app/editor/line-editor/single-line-editors/typography-view/typography-view.component.ts
@@ -6,6 +6,7 @@ export class TypographyType {
   constructor(
     public char: string,
     public classes: string,
+    public prevChar = null,
   ) {
   }
 }
@@ -31,6 +32,7 @@ export class TypographyViewComponent extends TextViewComponent implements OnInit
   private typographyTypes = new Array<TypographyType>(
     new TypographyType('g', ''),
     new TypographyType('p', 'small'),
+    new TypographyType('h', 'super small', 'p'),
     new TypographyType('h', 'super'),
   );
 
@@ -38,17 +40,16 @@ export class TypographyViewComponent extends TextViewComponent implements OnInit
   getTypographyView(): Array<TypographyView> {
     if (this.typographySentence.text.length === 0) { return []; }
     let currentType = this.typographySentence.text[0];
+    let prevType = '';
     let startPos = 0;
     const l = Math.min(this.typographySentence.text.length, this.sentence.text.length)
     const out = new Array<TypographyView>();
     for (let i = 1; i <= l; i++) {
-      if (i < l && this.typographySentence.isSeparatorAt(i)) {
-        continue;
-      }
       if (i === l || currentType !== this.typographySentence.text[i]) {
         let text = this.sentence.text.substring(startPos, i);
         text = text.replace(' ', '&nbsp;');
-        out.push(new TypographyView(text, this.typeByChar(currentType)));
+        out.push(new TypographyView(text, this.typeByChar(currentType, prevType)));
+        prevType = currentType;
         currentType = this.typographySentence.text[i];
         startPos = i;
       }
@@ -56,9 +57,13 @@ export class TypographyViewComponent extends TextViewComponent implements OnInit
     return out;
   }
 
-  typeByChar(c: string): TypographyType {
+  typeByChar(c: string, p: string): TypographyType {
     for (const t of this.typographyTypes) {
-      if (t.char === c) { return t; }
+      if (t.prevChar) {
+        if (t.char === c && t.prevChar === p) { return t; }
+      } else {
+        if (t.char === c) { return t; }
+      }
     }
     return new TypographyType('', '');
   }
